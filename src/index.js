3a5e85b1eb05e35ad57b93dfc4759ebe57db38ca
Dang Khoa <ndk11082@gmail.com>
2024-12-30 17:30:37 +0700

    test

--- src/index.js
+++ src/index.js
@@ -19,14 +19,37 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('messageCreate', (message) => {
+client.on('messageCreate', async (message) => {
   if (message.author.bot) return;
+
   if (message.content === '!ping') {
     message.reply('Ping Ping Pong! I am a bot.');
   }
 
   if (message.content === 'hello') {
-    message.reply(`Hello ${message.author.globalName}!`);
+    message.reply(`Hello ${message.author.globalName || message.author.username}!`);
+  }
+
+  if (message.content === '!list-users') {
+    const guild = message.guild;
+    if (!guild) {
+      return message.reply('Lệnh này chỉ có thể dùng trong server!');
+    }
+
+    try {
+      const members = await guild.members.fetch();
+
+      const memberList = members.map(member => member.user.tag).join('\n');
+
+      if (memberList.length > 2000) {
+        return message.reply('Danh sách quá dài, không thể gửi đầy đủ!');
+      }
+
+      message.reply(`Danh sách thành viên:\n${memberList}`);
+    } catch (error) {
+      console.error(error);
+      message.reply('Có lỗi khi lấy danh sách thành viên.');
+    }
   }
 });
 
